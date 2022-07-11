cad5255462b22ae267acda6993e163365de61148
Riku Virtanen <virtanen.riku91@gmail.com>
2022-07-11 12:03:35 +0300

    Fixed documentation and cleanup method

--- Frends.Files.LocalBackup/Frends.Files.LocalBackup/Definitions/Result.cs
+++ Frends.Files.LocalBackup/Frends.Files.LocalBackup/Definitions/Result.cs
@@ -24,8 +24,8 @@ public class Result
     /// <example>
     /// "Backups": 
     /// [
-	///	    "Backup": "Backup complete: C:\\test\\localbackup\\test - Copy (2).txt to C:\\test\\backup\\2022-07-07_08_51_02-e7e34166-f4fd-45e5-9307-ea5c2cf8e037\\test - Copy (2).txt",
-	///	    "Backup": "Backup complete: C:\\test\\localbackup\\test - Copy (3).txt to C:\\test\\backup\\2022-07-07_08_51_02-e7e34166-f4fd-45e5-9307-ea5c2cf8e037\\test - Copy (3).txt"
+	///	    "Backup complete: C:\\test\\localbackup\\test - Copy (2).txt to C:\\test\\backup\\2022-07-07_08_51_02-e7e34166-f4fd-45e5-9307-ea5c2cf8e037\\test - Copy (2).txt",
+	///	    "Backup complete: C:\\test\\localbackup\\test - Copy (3).txt to C:\\test\\backup\\2022-07-07_08_51_02-e7e34166-f4fd-45e5-9307-ea5c2cf8e037\\test - Copy (3).txt"
     /// ]
     /// </example>
     public List<string> Backups { get; private set; }
@@ -36,8 +36,8 @@ public class Result
     /// <example>
     /// "Cleanups: 
     /// [
-	///	    "Cleanup": "C:\\test\\backup\\2022-07-11_05_44_41-ab214387-98f0-44de-8ec5-16ba01b8ab97 deleted.",
-	///	    "Cleanup": "C:\\test\\backup\\2022-06-25_06_23_56-ab214387-98f0-44de-8ec5-16ba01b8ab97 deleted."
+	///	    "C:\\test\\backup\\2022-07-11_05_44_41-ab214387-98f0-44de-8ec5-16ba01b8ab97 deleted.",
+	///	    "C:\\test\\backup\\2022-06-25_06_23_56-ab214387-98f0-44de-8ec5-16ba01b8ab97 deleted."
     /// ]
     /// </example>
     public List<string> Cleanups { get; private set; }
