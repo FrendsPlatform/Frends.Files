889ab0feefce139fae1a420d0687a0b8fdb75bc5
Riku Virtanen <virtanen.riku91@gmail.com>
2022-07-11 10:48:30 +0300

    Removed BAckupObject class, changed the Result object to have list of string for backups and cleanups

--- Frends.Files.LocalBackup/CHANGELOG.md
+++ Frends.Files.LocalBackup/CHANGELOG.md
@@ -1,11 +1,12 @@
 # Changelog
 
-## [1.0.1] - 2022-07-07
+## [2.0.0] - 2022-07-11
 ### Added
 - Modified the task to use #process.executionid instead of a random guid
-- Added input for TaskExecutionId as guid
+- [Breaking change] Added input for TaskExecutionId as guid
 - Added backup directory value to Result object
 - Added file count how many were made backup of
+- [Breaking change] Removed BackupObject class and changed the result object to have list of string for backup and cleanup
 
 ## [1.0.0] - 2022-05-16
 ### Added
--- Frends.Files.LocalBackup/Frends.Files.LocalBackup/Definitions/Result.cs
+++ Frends.Files.LocalBackup/Frends.Files.LocalBackup/Definitions/Result.cs
@@ -19,43 +19,33 @@ public class Result
     public int FileCountInBackup { get; private set; }
 
     /// <summary>
-    /// Backup results.
+    /// Backup results as list of strings.
     /// </summary>
     /// <example>
-    /// "Backups": [
-	///     {
-	///	        "Backup": "Backup complete: C:\\test\\localbackup\\test - Copy (2).txt to C:\\test\\backup\\2022-07-07_08_51_02-e7e34166-f4fd-45e5-9307-ea5c2cf8e037\\test - Copy (2).txt",
-	///	        "Cleanup": null
-	///     },
-	///     {
-	///	        "Backup": "Backup complete: C:\\test\\localbackup\\test - Copy (3).txt to C:\\test\\backup\\2022-07-07_08_51_02-e7e34166-f4fd-45e5-9307-ea5c2cf8e037\\test - Copy (3).txt",
-	///	        "Cleanup": null
-	///     }
+    /// "Backups": 
+    /// [
+	///	    "Backup": "Backup complete: C:\\test\\localbackup\\test - Copy (2).txt to C:\\test\\backup\\2022-07-07_08_51_02-e7e34166-f4fd-45e5-9307-ea5c2cf8e037\\test - Copy (2).txt",
+	///	    "Backup": "Backup complete: C:\\test\\localbackup\\test - Copy (3).txt to C:\\test\\backup\\2022-07-07_08_51_02-e7e34166-f4fd-45e5-9307-ea5c2cf8e037\\test - Copy (3).txt"
     /// ]
     /// </example>
-    public List<BackupObject> Backups { get; private set; }
+    public List<string> Backups { get; private set; }
 
     /// <summary>
-    /// Cleanup results.
+    /// Cleanup results as list of strings.
     /// </summary>
     /// <example>
-    /// "Cleanups: [
-	///     {
-	///	        "Backup": null,
-	///	        "Cleanup": "C:\\test\\backup\\2022-07-11_05_44_41-ab214387-98f0-44de-8ec5-16ba01b8ab97 deleted."
-	///     },
-    ///     {
-	///	        "Backup": null,
-	///	        "Cleanup": "C:\\test\\backup\\2022-06-25_06_23_56-ab214387-98f0-44de-8ec5-16ba01b8ab97 deleted."
-	///     }
+    /// "Cleanups: 
+    /// [
+	///	    "Cleanup": "C:\\test\\backup\\2022-07-11_05_44_41-ab214387-98f0-44de-8ec5-16ba01b8ab97 deleted.",
+	///	    "Cleanup": "C:\\test\\backup\\2022-06-25_06_23_56-ab214387-98f0-44de-8ec5-16ba01b8ab97 deleted."
     /// ]
     /// </example>
-    public List<BackupObject> Cleanups { get; private set; }
+    public List<string> Cleanups { get; private set; }
 
-    internal Result(string directory, int fileCount, List<BackupObject> backups, List<BackupObject> cleanups)
+    internal Result(string directory, List<string> backups, List<string> cleanups)
     {
         Directory = directory;
-        FileCountInBackup = fileCount;
+        FileCountInBackup = backups.Count;
         Backups = backups;
         Cleanups = cleanups;
     }
