5eb6b57a49da075d78fbbc703b0ad9582d738d00
Riku Virtanen <virtanen.riku91@gmail.com>
2023-02-13 14:14:44 +0200

    Initial implementation

---
+++ Frends.Files.Delete/Frends.Files.Delete/Definitions/Input.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Frends.Files.Delete.Definitions
+{
+    /// <summary>
+    /// Input parameters.
+    /// </summary>
+    public class Input
+    {
+        /// <summary>
+        /// Source directory.
+        /// </summary>
+        /// <example>c:\temp</example>
+        [DisplayFormat(DataFormatString = "Text")]
+        public string Directory { get; set; }
+
+        /// <summary>
+        /// Pattern to match for files. The file mask uses regular expressions, but for convenience, it has special handling for * and ? wildcards.
+        /// </summary>
+        /// <example>test.txt, test*.txt, test?.txt, test.(txt|xml), test.[^t][^x][^t], &lt;regex&gt;^(?!prof).*_test.txt</example>
+        [DisplayFormat(DataFormatString = "Text")]
+        [DefaultValue("\"**\\Folder\\*.xml\"")]
+        public string Pattern { get; set; }
+    }
+}
