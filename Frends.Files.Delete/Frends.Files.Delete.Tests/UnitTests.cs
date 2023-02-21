7bd7a7336db33be4fa959146b055a1d6f6fa1051
Riku Virtanen <virtanen.riku91@gmail.com>
2023-02-21 08:37:51 +0200

    Restored Delete task

---
+++ Frends.Files.Delete/Frends.Files.Delete.Tests/UnitTests.cs
@@ -0,0 +1,88 @@
+using Frends.Files.Delete.Definitions;
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace Frends.Files.Delete.Tests;
+
+[TestFixture]
+public class UnitTests
+{
+    private readonly string _dir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../TestData/");
+    Input? _input;
+    Options? _options;
+
+    [SetUp]
+    public void Setup()
+    {
+        Helper.CreateTestFiles(_dir);
+
+        _input = new Input
+        {
+            Directory = _dir,
+            Pattern = "*"
+        };
+
+        _options = new Options
+        {
+            UseGivenUserCredentialsForRemoteConnections = false
+        };
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Helper.DeleteTestFolder(_dir);
+    }
+
+    [Test]
+    public void FileDeleteAll()
+    {
+        var result = Files.Delete(_input, _options, default);
+
+        Assert.AreEqual(7, result.Files.Count);
+        Assert.IsFalse(File.Exists(result.Files[0].Path));
+    }
+
+    [Test]
+    public void FileDeleteWithPattern()
+    {
+        var result = Files.Delete(
+            new Input
+            {
+                Directory = _dir,
+                Pattern = "Test1*"
+            }, _options, default);
+
+        Assert.AreEqual(2, result.Files.Count);
+        Assert.IsFalse(File.Exists(result.Files[0].Path));
+    }
+
+    [Test]
+    public void FileDeleteShouldNotThrowIfNoFilesFound()
+    {
+        var result = Files.Delete(
+            new Input()
+            {
+                Directory = _dir,
+                Pattern = "**/*.unknown"
+            },
+            _options,
+            default);
+
+        Assert.IsEmpty(result.Files);
+    }
+
+    [Test]
+    public void FileDeleteShouldThrowIfDirectoryIsNotFound()
+    {
+        var input = new Input()
+        {
+            Directory = @"F:\directory\that\dont\exists",
+            Pattern = "**/*.unknown"
+        };
+
+        var ex = Assert.Throws<DirectoryNotFoundException>(() => Files.Delete(input, _options, default));
+        Assert.AreEqual($"Directory does not exist or you do not have read access. Tried to access directory '{input.Directory}'", ex.Message);
+    }
+}
---
+++ Frends.Files.Delete/Frends.Files.Delete/Definitions/FileItem.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace Frends.Files.Delete.Definitions;
+
+/// <summary>
+/// Class for return items.
+/// </summary>
+public class FileItem
+{
+    /// <summary>
+    /// Name of the deleted file.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Full path of the deleted file.
+    /// </summary>
+    public string Path { get; set; }
+
+    /// <summary>
+    /// Size of the deleted file in mega bytes.
+    /// </summary>
+    public double SizeInMegaBytes { get; set; }
+
+    internal FileItem(FileInfo file)
+    {
+        Name = file.Name;
+        Path = file.FullName;
+        SizeInMegaBytes = file.Length / 1024d / 1024d;
+    }
+}
+
