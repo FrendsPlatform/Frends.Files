5b1b646613d6cc506cbd08e1e94203494380ecf7
Riku Virtanen <virtanen.riku91@gmail.com>
2023-02-28 16:17:54 +0200

    Initial implementation

---
+++ Frends.Files.Read/Frends.Files.Read/Definitions/Result.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+namespace Frends.Files.Read.Definitions;
+
+/// <summary>
+/// Write result class.
+/// </summary>
+public class Result
+{
+    /// <summary>
+    /// File content.
+    /// </summary>
+    /// <example>This is a test file.</example>
+    public string Content { get; set; }
+
+    /// <summary>
+    /// Full path to the file.
+    /// </summary>
+    /// <example>c:\temp\foo.txt</example>
+    public string Path { get; private set; }
+
+    /// <summary>
+    /// Size of the written file in mega bytes.
+    /// </summary>
+    /// <example>32</example>
+    public double SizeInMegaBytes { get; private set; }
+
+    /// <summary>
+    /// DateTime when file was created.
+    /// </summary>
+    /// <example></example>
+    public DateTime CreationTime { get; set; }
+
+    /// <summary>
+    /// DateTime for last write time of the file.
+    /// </summary>
+    /// <example></example>
+    public DateTime LastWriteTime { get; set; }
+
+    internal Result(FileInfo info, string content)
+    {
+        Content = content;
+        Path = info.FullName;
+        SizeInMegaBytes = Math.Round(info.Length / 1024d / 1024d, 3);
+        CreationTime = info.CreationTime;
+        LastWriteTime = info.LastWriteTime;
+    }
+}
