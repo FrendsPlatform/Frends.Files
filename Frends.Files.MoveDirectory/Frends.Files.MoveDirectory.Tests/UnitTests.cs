08d92c00c67c83d40009f9f2ada0fe1a018674a5
Vertti Ahlstén <vertti.ahlsten@hiq.fi>
2023-08-16 16:37:55 +0300

    Tests updated

--- Frends.Files.MoveDirectory/Frends.Files.MoveDirectory.Tests/UnitTests.cs
+++ Frends.Files.MoveDirectory/Frends.Files.MoveDirectory.Tests/UnitTests.cs
@@ -3,6 +3,7 @@ using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Assert = NUnit.Framework.Assert;
 using Frends.Files.MoveDirectory.Definitions;
+using NUnit.Framework;
 
 namespace Frends.Files.MoveDirectory.Tests;
 
@@ -24,44 +25,69 @@ public class UnitTests
     }
 
     [TestMethod]
-    public void CreateFolderShouldCreateWholePath()
+    public void MoveShouldThrowIfDestinationFolderExistsAndBehaviourIsThrow()
     {
-        var newPath = Path.Combine(_context.RootPath, "temp\\foo\\bar");
-        var result = Files.CreateDirectory(new Input() { Directory = newPath }, new Options() { UseGivenUserCredentialsForRemoteConnections = false });
-        Assert.AreEqual(result.Path, newPath);
+        _context.CreateFiles("temp/foo/bar/foo.txt");
+        _context.CreateFiles("temp/bar/foo.txt");
+        var sourcePath = Path.Combine(_context.RootPath, "temp\\foo\\bar");
+        var targetPath = Path.Combine(_context.RootPath, "temp\\bar");
+        var ex = Assert.Throws<IOException>(() => Files.MoveDirectory(new Input() { SourceDirectory = sourcePath, TargetDirectory = targetPath }, new Options() { IfTargetDirectoryExists = DirectoryExistsAction.Throw }));
+        Assert.IsTrue(ex.Message.ToString().Contains("Cannot create a file when that file already exists"));
     }
 
     [TestMethod]
-    public void CreateFolderShouldDoNothingIfPathExists()
+    public void MoveShouldCreateCopyIfDestinationFolderExistsAndBehaviourIsCopy()
     {
         _context.CreateFiles("temp/foo/bar/foo.txt");
-        var newPath = Path.Combine(_context.RootPath, "temp\\foo\\bar");
-        var result = Files.CreateDirectory(new Input() { Directory = newPath }, new Options() { UseGivenUserCredentialsForRemoteConnections = false });
-        Assert.AreEqual(result.Path, newPath);
+        _context.CreateFiles("temp/bar/foo.txt");
+        var sourcePath = Path.Combine(_context.RootPath, "temp\\foo\\bar");
+        var targetPath = Path.Combine(_context.RootPath, "temp\\bar");
+        var result = Files.MoveDirectory(new Input() { SourceDirectory = sourcePath, TargetDirectory = targetPath }, new Options() { IfTargetDirectoryExists = DirectoryExistsAction.Rename });
+        Assert.AreEqual(result.TargetPath, targetPath + "(1)");
+    }
+
+    [Test]
+    public void MoveShouldOverwriteIfDestinationFolderExistsAndBehaviourIsOverwrite()
+    {
+        _context.CreateFiles("temp/foo/bar/foo.txt");
+        _context.CreateFiles("temp/bar/foo.txt");
+        var sourcePath = Path.Combine(_context.RootPath, "temp\\foo\\bar");
+        var targetPath = Path.Combine(_context.RootPath, "temp\\bar");
+
+        var result = Files.MoveDirectory(new Input() { SourceDirectory = sourcePath, TargetDirectory = targetPath }, new Options() { IfTargetDirectoryExists = DirectoryExistsAction.Overwrite });
+        Assert.AreEqual(result.TargetPath, targetPath);
     }
 
     [TestMethod]
     [ExpectedException(typeof(ArgumentException))]
     public void ThrowUsernameInvalidError()
     {
-        var newPath = Path.Combine(_context.RootPath, "temp\\foo\\bar");
-        var result = Files.CreateDirectory(new Input() { Directory = newPath }, new Options() { UseGivenUserCredentialsForRemoteConnections = true, UserName = "domain/example", Password = "Password123" });
+        _context.CreateFiles("temp/foo/bar/foo.txt");
+        _context.CreateFiles("temp/bar/foo.txt");
+        var sourcePath = Path.Combine(_context.RootPath, "temp\\foo\\bar");
+        var targetPath = Path.Combine(_context.RootPath, "temp\\bar");
+
+        var result = Files.MoveDirectory(new Input() { SourceDirectory = sourcePath, TargetDirectory = targetPath }, new Options() { UseGivenUserCredentialsForRemoteConnections = true, UserName = "domain/example", Password = "Password123" });
         Assert.AreEqual("UserName field must be of format domain\\username was: domain/example", result);
     }
 
     [TestMethod]
     public void ThrowRemoteConnectionError()
     {
-        var newPath = Path.Combine(_context.RootPath, "temp\\foo\\bar");
-        var result = Files.CreateDirectory(new Input() { Directory = newPath }, new Options() { UseGivenUserCredentialsForRemoteConnections = true, UserName = "domain\\example", Password = "Password123" });
-        Assert.AreEqual(result.Path, newPath);
+        _context.CreateFiles("temp/foo/bar/foo.txt");
+        _context.CreateFiles("temp/bar/foo.txt");
+        var sourcePath = Path.Combine(_context.RootPath, "temp\\foo\\bar");
+        var targetPath = Path.Combine(_context.RootPath, "temp\\bar");
+
+        var result = Files.MoveDirectory(new Input() { SourceDirectory = sourcePath, TargetDirectory = targetPath }, new Options() { UseGivenUserCredentialsForRemoteConnections = true, UserName = "domain\\example", Password = "Password123" });
+        Assert.AreEqual(result.TargetPath, targetPath);
     }
 
     [TestMethod]
     [ExpectedException(typeof(ArgumentNullException))]
     public void ThrowInputEmpty()
     {
-        var result = Files.CreateDirectory(new Input() { }, new Options() { });
+        var result = Files.MoveDirectory(new Input() { }, new Options() { });
         Assert.AreEqual("Directory cannot be empty.", result);
     }
 }
