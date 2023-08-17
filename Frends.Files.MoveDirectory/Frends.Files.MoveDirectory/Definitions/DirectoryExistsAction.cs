fc17db3d5832e9fddd42496799358e3b9d4f1933
Vertti Ahlstén <vertti.ahlsten@hiq.fi>
2023-08-17 15:49:42 +0300

    Documentation updated

--- Frends.Files.MoveDirectory/Frends.Files.MoveDirectory/Definitions/DirectoryExistsAction.cs
+++ Frends.Files.MoveDirectory/Frends.Files.MoveDirectory/Definitions/DirectoryExistsAction.cs
@@ -8,10 +8,12 @@ public enum DirectoryExistsAction {
     /// Throw error on default.
     /// </summary>
     Throw,
+
     /// <summary>
     /// Create a new directory with a name that appends a number to the end, e.g. "directory(2)".
     /// </summary>
     Rename,
+
     /// <summary>
     /// Overwrite the target directory, by removing it first before moving the source directory.
     /// </summary>
